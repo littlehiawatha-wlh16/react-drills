843938686019befd334bb9532f24095c16c3546d
Jacob Hall <halljaco@yahoo.com>
2020-01-29 16:36:11 -0700

    reactDrills_Part2_complete 3.3.a

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+  class App extends Component{
+    constructor(){
+      super();
 
-function App() {
+        this.state = {
+          hobbies: [ 'hiking', 'camping', 'fishing', 'crabing', 'biking']
+        };
+  }
+  
+
+  render () {
+    let hobbiesDisplayed = this.state.hobbies.map((element,index) => {
+    return <h2 key = {index} > {element}</h2>;
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >{hobbiesDisplayed}</h1>
+      
+
     </div>
   );
 }
 
+}
 export default App;
+
+
+/// Where is state initialized? initial state or were we start
+/// where is state changed?
+/// where is state displayed?
