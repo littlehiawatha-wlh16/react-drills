b71b1c335a518d33b9c81d7bd686fe80e2ce5767
Jacob Hall <halljaco@yahoo.com>
2020-01-29 15:52:01 -0700

    finished app1_3.3.a_reactDrills

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      messages: ''
+    };
+
+  }
+
+  // handleChange(value) {
+  //   this.setState({message: value});
+  // }
+
+  handleChange = (value) => {
+    this.setState({messages: value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange = { e => this.handleChange(e.target.value)} type = 'text'  />
+        <p>{this.state.messages} </p>  
+      </div>
+    );
+  }  
+  
 }
 
 export default App;
+
+
+// // ---------------------- solution Code ----------------------- // // 
+
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       message: ""
+//     };
+//   }
+
+//   handleChange(value) {
+//     this.setState({ message: value });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <input onChange={e => this.handleChange(e.target.value)} type="text" />
+//         <p>{this.state.message}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
