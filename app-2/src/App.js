a7671db06b0b735aa217fdc67f1fbc34007d2d34
Jacob Hall <halljaco@yahoo.com>
2020-01-30 22:33:20 -0700

    react-drill4 3.3.a

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -31,3 +31,30 @@ export default App;
 /// Where is state initialized? initial state or were we start
 /// where is state changed?
 /// where is state displayed?
+
+
+
+// // // --------------------------------> Solution Code <--------------------------------- // // 
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
+    return <div className="App">{foodsToDisplay}</div>;
+  }
+}
+
+export default App;
--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleUsernameChange(name) {
+    this.setState({ username: name });
+  }
+
+  handlePasswordChange(pass) {
+    this.setState({ password: pass });
+  }
+
+  handleLogin() {
+    alert(`Username: ${this.state.username} Password: ${this.state.password}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={e => this.handleUsernameChange(e.target.value)}
+          type="text"
+        />
+        <input
+          onChange={e => this.handlePasswordChange(e.target.value)}
+          type="text"
+        />
+        <button onClick={this.handleLogin}>Login</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Login;
