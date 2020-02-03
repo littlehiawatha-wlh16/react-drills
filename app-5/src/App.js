29f15982dee6503be994b45573c4b3fb654b258d
Jacob Hall <halljaco@yahoo.com>
2020-02-02 17:00:15 -0700

    react drills app 5 complete without url 3.3.a

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Image from './Components/Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className = 'App'>
+        <Image url = {'https://http.cat/409'} />
+        {/* <Image url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdev.to%2Fteamxenox%2Ftop-10-programming-memes-pemes-4php&psig=AOvVaw1z0rmUgfT19r0opZB3KeI3&ust=1580772935103000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCj65uEtOcCFQAAAAAdAAAAABAI'/> */} 
+        {/* how do I make the above url work? */}
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
